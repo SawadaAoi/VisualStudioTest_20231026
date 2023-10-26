3270bc431b8d9dfb29e1e55b4f37c53c7712edaa
SawadaAoi <misokatsu2234@icloud.com>
2023-10-26 17:39:50 +0900

    Source.cppの作成

---
+++ git_test_20231024/Source.cpp
@@ -0,0 +1,37 @@
+// ============================
+// CL22
+// ----------------------------
+// メイン用cpp
+// ----------------------------
+// main.cpp
+// ============================
+
+// --- 警告抑止 ---
+#define  _CRT_SECURE_NO_WARNINGS
+
+
+// --- インクルード部 ----
+#include <iostream>
+#include<crtdbg.h> // メモリリークチェック用
+
+
+// --- 定数定義 ---
+
+// --- 構造体宣言部 ---
+
+// --- グローバル宣言部 ---
+
+// --- プロトタイプ宣言部 ---
+
+using namespace std;
+
+int main(void)
+{
+	// プログラム終了時にメモリリークをチェックする
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	// 入力待ち
+	cin.ignore(2, '\n');
+	cin.get();
+	return 0;
+}
