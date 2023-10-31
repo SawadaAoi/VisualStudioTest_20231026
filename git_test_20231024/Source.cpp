f83d461d653d2559519786580736d55424c340f1
NiedaJunya <edaaa24@gmail,com>
2023-10-31 11:18:24 +0900

    mainのコミットのッを消した。

--- git_test_20231024/Source.cpp
+++ git_test_20231024/Source.cpp
@@ -37,7 +37,7 @@ int main(void)
 	std::cout << "Testコメント20231028!" << std::endl;
 	std::cout << "Testコメント20231028②!" << std::endl;
 	std::cout << "Testコメント20231029!" << std::endl;
-	std::cout << "正しいコミット" << std::endl;
+	std::cout << "正しいコミト" << std::endl;
 	std::cout << "featureB" << std::endl;
     std::cout << "featureB②" << std::endl;
 	std::cout << "featureC" << std::endl;
